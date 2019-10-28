77a160d3130373e8afc21175a6024c148c86fa24
Михман <miklblitz@yandex.ru>
2019-10-28 17:06:49 +0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>React App !!</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://10.162.113.18:3005/users')
+      .then(response=>response.json())
+      .then(users=>this.setState({'robots': users}))
+      .catch(function (error) {
+        console.log('Looks like there was a problem: \n', error);
+      });
+  }
+
+  render(){
+    return (
+      <div className="App" >
+       {
+         this.state.robots.map(robot => <h2 key={robot.id}>{robot.name}</h2>)
+       }
+      </div>
+    )
+  }
 }
 
 export default App;
