384b1a13c34c5513b806df5746fd634e0d9e4d14
Михман <miklblitz@yandex.ru>
2019-10-29 14:53:10 +0400

    Updates

---
+++ precache-manifest.76ff264e683fbc543c55c7f605acc279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b43f20133d545ca14307de34c5ac10",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "258aafd81156520d0dd6",
+    "url": "/robots/static/css/main.4bc1fb5c.chunk.css"
+  },
+  {
+    "revision": "48780a81820302651baa",
+    "url": "/robots/static/js/2.61f73c30.chunk.js"
+  },
+  {
+    "revision": "258aafd81156520d0dd6",
+    "url": "/robots/static/js/main.935dc178.chunk.js"
+  },
+  {
+    "revision": "13a7fef3b254855b4bc3",
+    "url": "/robots/static/js/runtime-main.d11597f1.js"
+  }
+]);
